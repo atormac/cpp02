967453f9392a30cc37a46a7d91c05b3362705051
atormac <atorma@student.hive.fi>
2024-10-04 18:42:01 +0300

    initial commit

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,12 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	_value = 0;
+	_bits = 8;
+}
+
+Fixed::~Fixed()
+{
+
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,15 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int _value;
+		int _bits;
+	public:
+		Fixed();
+		~Fixed();
+};
+
+#endif
+
