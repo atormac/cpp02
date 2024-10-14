d3277ed6176d5f7a5631e26e756fa985c20aa592
atormac <atorma@student.hive.fi>
2024-10-14 17:13:39 +0300

    min/max tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,13 +7,15 @@ int main(void)
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	Fixed c(1.0f);
-	std::cout << a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "b: " << b << std::endl;
+	std::cout << "max(a, b): " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min(a, b: " << Fixed::min( a, b ) << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
 
 	std::cout << std::endl;
 	std::cout << "a + b: " << a + b << std::endl;
