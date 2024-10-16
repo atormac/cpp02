0b09b24579f5304d66e26f5020e9289966aaf9f3
atormac <atorma@student.hive.fi>
2024-10-16 17:05:21 +0300

    print c

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,6 +7,8 @@ int main(void)
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	Fixed c(1.0f);
+
+	std::cout << "c: " << c << std::endl;
 	std::cout << "a: " << a << std::endl;
 	std::cout << "b: " << b << std::endl;
 	std::cout << "max(a, b): " << Fixed::max( a, b ) << std::endl;
